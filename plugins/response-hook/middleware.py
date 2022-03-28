eedfb878e56116412d4e957eec699bfde933780e
Dmitry Movchan <dmitry.movchan@icanbwell.com>
2022-03-28 22:03:43 +0300

    update multiple plugins

--- plugins/response-hook/middleware.py
+++ plugins/response-hook/middleware.py
@@ -5,6 +5,5 @@ from gateway import TykGateway as tyk
 @Hook
 def ResponseHook(request, response, session, metadata, spec):
     tyk.log("ResponseHook is called", "info")
-    tyk.log("ResponseHook: upstream returned >> {0}".format(response.status_code), "info")
     return response
 
