03fb862536c691a0696d9a506c833da992aff0fa
Dmitry Movchan <dmitry.movchan@icanbwell.com>
2022-03-28 20:56:57 +0300

    code upd; add dummy steps

--- plugins/first/middleware.py
+++ plugins/first/middleware.py
@@ -20,3 +20,4 @@ def ResponseHook(request, response, session, metadata, spec):
     tyk.log("ResponseHook is called", "info")
     tyk.log("ResponseHook: upstream returned >> {0}".format(response.status_code), "info")
     return response
+
